9ee36a7450dd5cea353126e17cfc5523edb079d0
= <=>
2019-04-16 10:42:53 -0400

    got basic idea of page working

--- routes/index.js
+++ routes/index.js
@@ -3,66 +3,53 @@ const router = express.Router();
 const data = require('./../public/ItemData.json');
 
 
-router.get('/subscribe',(req,res)=>{
-    //Articles, visits
-    let pageCount = [0,0];
-    if (req.cookies && req.cookies.pageCount)
-    {
-        pageCount[0] = parseInt(req.cookies.pageCount[0]);
-        pageCount[1] = parseInt(req.cookies.pageCount[1]);
-        //pageCount = parseInt(req.cookies.pageCount);
-    }
-    res.render('subscribe',{
-        timesVisited:pageCount[1]
-    });
-});
+//USE COOKIES TO STORE LAST USE DATA (like for a build).
 
-router.get('/:id',(req,res)=>{
-    //Articles, visits
-    let pageCount = [0,0];
-    if (req.cookies && req.cookies.pageCount)
-    {
-        pageCount[0] = parseInt(req.cookies.pageCount[0]);
-        pageCount[1] = parseInt(req.cookies.pageCount[1]);
-        //pageCount = parseInt(req.cookies.pageCount);
-    }
-    console.log("articles: "+pageCount[0]+"; visits: "+pageCount[1]);
-    //updating visit AND article count
-    if (pageCount[0]<3){
-        pageCount[0]++;
-        pageCount[1]++;
-        //updating cookie
-        res.cookie('pageCount', pageCount, {
-            maxAge : 1000*60*60*24*7
-        });
-        res.render('article',{
-            data:articles[parseInt(req.params.id)]
-        });
-    }
-    else {
-        res.redirect('/subscribe');
-    }
-});
+
+// router.get('/:id',(req,res)=>{
+//     //Articles, visits
+//     let pageCount = [0,0];
+//     if (req.cookies && req.cookies.pageCount)
+//     {
+//         pageCount[0] = parseInt(req.cookies.pageCount[0]);
+//         pageCount[1] = parseInt(req.cookies.pageCount[1]);
+//         //pageCount = parseInt(req.cookies.pageCount);
+//     }
+//     console.log("articles: "+pageCount[0]+"; visits: "+pageCount[1]);
+//     //updating visit AND article count
+//     if (pageCount[0]<3){
+//         pageCount[0]++;
+//         pageCount[1]++;
+//         //updating cookie
+//         res.cookie('pageCount', pageCount, {
+//             maxAge : 1000*60*60*24*7
+//         });
+//         res.render('article',{
+//             data:articles[parseInt(req.params.id)]
+//         });
+//     }
+// });
 
 
 
 router.get('/',(req,res)=>{
     //Articles, visits
-    let pageCount = [0,0];
-    if (req.cookies && req.cookies.pageCount)
-    {
-        pageCount[0] = parseInt(req.cookies.pageCount[0]);
-        pageCount[1] = parseInt(req.cookies.pageCount[1]);
-        //pageCount = parseInt(req.cookies.pageCount);
-    }
-    pageCount[1]++;
-    //updating cookie
-    res.cookie('pageCount',pageCount, {
-        maxAge : 1000*60*60*24*7
-    });
+
+    // let pageCount = [0,0];
+    // if (req.cookies && req.cookies.pageCount)
+    // {
+    //     pageCount[0] = parseInt(req.cookies.pageCount[0]);
+    //     pageCount[1] = parseInt(req.cookies.pageCount[1]);
+    //     //pageCount = parseInt(req.cookies.pageCount);
+    // }
+    // pageCount[1]++;
+    // //updating cookie
+    // res.cookie('pageCount',pageCount, {
+    //     maxAge : 1000*60*60*24*7
+    // });
+
     res.render('index',{
-        dataList:articles,
-        pageCount:pageCount[0]
+        data : data
     });
 });
 
